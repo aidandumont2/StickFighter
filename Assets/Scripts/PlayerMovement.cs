61d395743cfde49cae088d5c109be39a1bc81349
aidandumont2 <aidan.dumont@yahoo.com>
2023-02-10 17:22:10 +0100

    Mouvement fini

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    private float speed = 5f;
+    private float jumpspeed = 2f;
+
+    public Rigidbody2D rb;
+
     public SpriteRenderer spriterenderer;
     public Sprite spriteCrouch;
     public Sprite spriteStand;
+
+    private bool isGrounded = true;
+    private bool isCrouching = false;
     // Update is called once per frame
     void Update()
     {
+        //IsGrounded();
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Move(-speed);
+            if(isCrouching == false)
+            {
+                Move(-speed);
+            }
+            
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            Move(speed);
+            if (isCrouching == false)
+            {
+                Move(speed);
+            }
+            
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
@@ -26,10 +42,15 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             spriterenderer.sprite = spriteStand;
+            isCrouching = false;
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Move(speed);
+            if (isCrouching == false)
+            {
+                Jump();
+            }
+            
         }
     }
 
@@ -44,5 +65,43 @@ public class PlayerMovement : MonoBehaviour
     private void Crouch()
     {
         spriterenderer.sprite = spriteCrouch;
+        isCrouching = true;
+    }
+
+    private void Jump()
+    {
+            if (isGrounded == true)
+        {
+            speed = jumpspeed;
+            rb.AddForce(Vector2.up * 6.5f, (ForceMode2D)ForceMode2D.Impulse);
+            isGrounded = false;
+
+        }
+            
+    }
+
+    /*private void IsGrounded() {
+        Debug.Log("ground test");
+        if (Collision2D )
+        {
+            Debug.Log("is grounded true");
+            isGrounded = true;
+        }
+        else
+        {
+            Debug.Log("ground false");
+            isGrounded = false;
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ground test");
+        if (collision.gameObject.name == "Sol")
+        {
+            isGrounded = true;
+            speed = 5f;
+        }
+        
     }
 }

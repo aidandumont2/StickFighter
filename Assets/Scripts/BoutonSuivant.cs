da96e04f1b0938d1aa19c72ba407c2e5de1c992e
RFENZL1 <92161747+RFENZL@users.noreply.github.com>
2023-03-13 17:39:03 +0100

    Choix camp fini

---
+++ Assets/Scripts/BoutonSuivant.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoutonSuivant : MonoBehaviour
+{
+    public string sceneSuivante;
+    public GameObject joueur1;
+    public GameObject joueur2;
+    public void suivant() 
+    {
+        if ((joueur1.transform.position.x == 6 && joueur2.transform.position.x == - 6) || (joueur1.transform.position.x == -6 && joueur2.transform.position.x == 6))
+        {
+            Debug.Log("lol");
+            SceneManager.LoadScene(sceneSuivante);
+        }
+    }
+}

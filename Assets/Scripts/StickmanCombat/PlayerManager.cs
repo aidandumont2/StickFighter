87b9df331581ab4d12d534958ddf87838139caf3
aidandumont2 <aidan.dumont@yahoo.com>
2023-04-17 17:54:30 +0200

    regen stamina

--- Assets/Scripts/StickmanCombat/PlayerManager.cs
+++ Assets/Scripts/StickmanCombat/PlayerManager.cs
@@ -22,9 +22,11 @@ public class PlayerManager : MonoBehaviour
     public int lifepoint = 100;
     public int stamina = 7;
 
+    private float tempsRegenStamina = 2.00f;
     
     void Update()
     {
+        RegenStamina();
         lifeBar.value = lifepoint;
         staminaBar.value = stamina;
         if (lifepoint <= 0)
@@ -46,5 +48,19 @@ public class PlayerManager : MonoBehaviour
         }
         collision.gameObject.SetActive(false);
         refTMP.text = numPlayer;
+    }
+
+    private void RegenStamina()
+    {
+        if (stamina != 7)
+        {
+            tempsRegenStamina -= Time.deltaTime;
+            if(tempsRegenStamina <= 0.00f)
+            {
+               stamina += 1;
+               tempsRegenStamina = 2.00f;
+            }
+        }
+        
     }
 }
